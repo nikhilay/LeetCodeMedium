177922d25801da40c6faa56e6cb495ce904160c2
nikhilay <nikhilayyagari15@gmail.com>
2016-10-24 15:00:09 -0400

    Find leaves of binary tree solution

---
+++ src/FindLeavesOfBinaryTree.java
@@ -0,0 +1,67 @@
+/**
+ * Created by Nikhil on 10/24/16.
+ *
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Given a binary tree, find all leaves and then remove those leaves. Then repeat the previous steps until the tree is empty.
+ * Example:
+ * Given binary tree
+ * 1
+ * / \
+ * 2   3
+ * / \
+ * 4   5
+ * Returns [4, 5, 3], [2], [1].
+ * Explanation:
+ * 1. Remove the leaves [4, 5, 3] from the tree
+ * 1
+ * /
+ * 2
+ * 2. Remove the leaf [2] from the tree
+ * 1
+ * 3. Remove the leaf [1] from the tree
+ * []
+ * Returns [4, 5, 3], [2], [1].
+ */
+public class FindLeavesOfBinaryTree {
+    /**
+     * Definition for a binary tree node.
+     */
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<List<Integer>> findLeaves(TreeNode root) {
+        List<List<Integer>> result = new LinkedList<>();
+        findLeavesAndRemove(result, root);
+        return result;
+    }
+
+    private int findLeavesAndRemove(List<List<Integer>> result, TreeNode root) {
+        if (root == null) {
+            return -1;
+        }
+        if (root.left == null && root.right == null) {
+            result.get(0).add(root.val);
+            return 1;
+        }
+
+        int leftResult = findLeavesAndRemove(result, root.left);
+        int rightResult = findLeavesAndRemove(result, root.right);
+
+        int max = Math.max(leftResult, rightResult);
+        root.left = null;
+        root.right = null;
+
+        result.get(max).add(root.val);
+        return max + 1;
+    }
+}
