ca74255bb69fa0ba7448a9454fab2a9e00566ee5
nikhilay <nikhilayyagari15@gmail.com>
2016-10-26 22:27:43 -0400

    Removed unused code

--- src/IntegerToRoman.java
+++ src/IntegerToRoman.java
@@ -1,27 +1,9 @@
 /**
  * Created by Nikhil on 10/26/16.
  */
-
-import java.util.HashMap;
-
-import static com.sun.javafx.css.SizeUnits.CM;
-import static javafx.scene.input.KeyCode.M;
-import static javax.swing.UIManager.put;
-
 /**
  * Given an integer, convert it to a roman numeral.
  * Input is guaranteed to be within the range from 1 to 3999.
- */
-
-/**
- *         HashMap<Integer, Character> hmap = new HashMap<>();
- hmap.put(1, 'I');
- hmap.put(5, 'V');
- hmap.put(10, 'X');
- hmap.put(50, 'L');
- hmap.put(100, 'C');
- hmap.put(500, 'D');
- hmap.put(1000, 'M');
  */
 public class IntegerToRoman {
     public String intToRoman(int num) {
