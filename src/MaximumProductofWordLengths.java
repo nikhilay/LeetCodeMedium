e504cc93c0a73dc68e602566f0e357c583839dbe
nikhilay <nikhilayyagari15@gmail.com>
2016-10-29 18:23:58 -0400

    Maximum product of word lengths solution

--- src/MaximumProductofWordLengths.java
+++ src/MaximumProductofWordLengths.java
@@ -6,17 +6,14 @@
  * Given a string array words, find the maximum value of length(word[i]) * length(word[j])
  * where the two words do not share common letters. You may assume that each word will
  * contain only lower case letters. If no such two words exist, return 0.
- * <p>
  * Example 1:
  * Given ["abcw", "baz", "foo", "bar", "xtfn", "abcdef"]
  * Return 16
  * The two words can be "abcw", "xtfn".
- * <p>
  * Example 2:
  * Given ["a", "ab", "abc", "d", "cd", "bcd", "abcd"]
  * Return 4
  * The two words can be "ab", "cd".
- * <p>
  * Example 3:
  * Given ["a", "aa", "aaa", "aaaa"]
  * Return 0
