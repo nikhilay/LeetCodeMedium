cdc39f363906605e63f2f87f5133ed6a0a980cb0
nikhilay <nikhilayyagari15@gmail.com>
2016-10-29 12:01:44 -0400

    Battle ship in board solution

---
+++ src/BattleshipsInABoard.java
@@ -0,0 +1,34 @@
+/**
+ * Created by Nikhil on 10/28/16.
+ */
+
+/**
+ * Given an 2D board, count how many different battleships are in it.
+ * The battleships are represented with 'X's, empty slots are represented with '.'s.
+ * You may assume the following rules:
+ * You receive a valid board, made of only battleships or empty slots.
+ * Battleships can only be placed horizontally or vertically. In other words,
+ * they can only be made of the shape 1xN (1 row, N columns) or Nx1 (N rows, 1 column), where N can be of any size.
+ * At least one horizontal or vertical cell separates between two battleships - there are no adjacent battleships.
+ */
+
+public class BattleshipsInABoard {
+    public static void main(String[] args) {
+        char[][] input = {{'X','.','.','X'},{'.','.','.','X'},{'.','.','.','X'}};
+        System.out.println(new BattleshipsInABoard().countBattleships(input));
+    }
+
+    public int countBattleships(char[][] board) {
+        if (board == null || board.length == 0) return 0;
+        int count = 0;
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[0].length; col++) {
+                if (board[row][col] == '.') continue;
+                if (row > 0 && board[row - 1][col] == 'X') continue;
+                if (col > 0 && board[row][col - 1] == 'X') continue;
+                count++;
+            }
+        }
+        return count;
+    }
+}
