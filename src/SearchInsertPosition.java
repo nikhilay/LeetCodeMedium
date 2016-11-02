4bc94936adba24ef9b2d40230c66fd7c97c5964e
nikhilay <nikhilayyagari15@gmail.com>
2016-11-02 00:41:34 -0400

    Search insert position solution

---
+++ src/SearchInsertPosition.java
@@ -0,0 +1,39 @@
+/**
+ * Created by Nikhil on 11/2/16.
+ */
+
+/**
+ * Given a sorted array and a target value, return the index if the target is found.
+ * If not, return the index where it would be if it were inserted in order.
+ * You may assume no duplicates in the array.
+ * Here are few examples.
+ * [1,3,5,6], 5 → 2
+ * [1,3,5,6], 2 → 1
+ * [1,3,5,6], 7 → 4
+ * [1,3,5,6], 0 → 0
+ */
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+        int[] input = {1, 3};
+        new SearchInsertPosition().searchInsert(input, 2);
+    }
+
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return 0;
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (nums[mid] == target) {
+                return mid;
+            } else if (target > nums[mid]) {
+                low = mid + 1;
+            } else if (target < nums[mid]) {
+                high = mid - 1;
+            }
+        }
+
+        return low;
+    }
+
+}
