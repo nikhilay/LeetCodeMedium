39ccee74bc1fcf52082211108d8e2d41a883c4d3
nikhilay <nikhilayyagari15@gmail.com>
2016-11-17 16:55:29 -0500

    LinkedList cycle two solution

---
+++ src/LinkedListCycleTwo.java
@@ -0,0 +1,63 @@
+import java.util.HashSet;
+
+/**
+ * Created by Nikhil on 11/17/16.
+ */
+
+/**
+ * Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+ * Note: Do not modify the linked list.
+ * Follow up:
+ * Can you solve it without using extra space?
+ */
+public class LinkedListCycleTwo {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    //With Extra space
+    public ListNode detectCycleWithExtraSpace(ListNode head) {
+        if (head == null) return null;
+        HashSet<ListNode> hset = new HashSet<>();
+        while (head != null) {
+            if (!hset.contains(head)) {
+                hset.add(head);
+            } else {
+                return head;
+            }
+            head = head.next;
+        }
+
+        return null;
+    }
+
+    //Without Extra space
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(slow ==fast){
+                ListNode temp = head;
+                while(slow!=temp){
+                    slow = slow.next;
+                    temp = temp.next;
+                }
+                return slow;
+            }
+
+        }
+        return null;
+    }
+
+}
