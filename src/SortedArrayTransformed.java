ee8df942225bc3969fb869bfd687ebd968b9bf5b
nikhilay <nikhilayyagari15@gmail.com>
2017-01-03 15:53:10 -0500

    fixed a bug

--- src/SortedArrayTransformed.java
+++ src/SortedArrayTransformed.java
@@ -15,14 +15,8 @@
  */
 
 public class SortedArrayTransformed {
-    public static void main(String[] args) {
-        int[] input = {-4, -2, 2, 4};
-
-        new SortedArrayTransformed().SortTransformedArray(input, 1, 3, 5);
-    }
-
-    public void SortTransformedArray(int[] nums, int a, int b, int c) {
-        long[] result = new long[nums.length];
+    public int[] sortTransformedArray(int[] nums, int a, int b, int c) {
+        int[] result = new int[nums.length];
         int n = nums.length;
         int index = a >= 0 ? n - 1 : 0;
         int i = 0;
@@ -31,21 +25,16 @@ public class SortedArrayTransformed {
             if (a >= 0) {
                 result[index--] = cal(nums[i], a, b, c) > cal(nums[j], a, b, c) ? cal(nums[i++], a, b, c) : cal(nums[j--], a, b, c);
             } else {
-                result[index++] = cal(nums[i], a, b, c) > cal(nums[j], a, b, c) ? cal(nums[i++], a, b, c) : cal(nums[j--], a, b, c);
+                result[index++] = cal(nums[i], a, b, c) > cal(nums[j], a, b, c) ? cal(nums[j--], a, b, c) : cal(nums[i++], a, b, c);
             }
 
+
         }
-        print(result);
+        return result;
 
     }
 
     private int cal(int x, int a, int b, int c) {
         return a * x * x + b * x + c;
-    }
-
-    private void print(long[] input) {
-        for (long r : input) {
-            System.out.print(r + ", ");
-        }
     }
 }
